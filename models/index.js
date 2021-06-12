ee5f31f124be24adcaaaef5568b0a301565bf52e
gannacon <cwgannaway@gmail.com>
2021-06-11 22:15:54 -0500

    populate tag routes

--- models/index.js
+++ models/index.js
@@ -16,10 +16,10 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: "product_id" });
+Product.belongsToMany(Tag, { through: "product_tag" });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: "tag_id" });
+Tag.belongsToMany(Product, { through: "product_tag" });
 
 module.exports = {
   Product,
